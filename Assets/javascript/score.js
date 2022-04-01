7ff226e9f5902de7cedf73855143ac0ce7154c0a
Mike Sands <mikesands94@gmail.com>
2022-04-01 16:17:07 -0400

    entered html section given via hw startup

---
+++ Assets/javascript/score.js
@@ -0,0 +1,14 @@
+//Acceptance Criteria:
+//TO DO: Make a start button
+    //Make a "VIEW HIGHSCORES" button
+
+//TO DO: Timer starts when start button is clicked, first question is presented
+
+//TO DO: Once question is answered, present with another question
+    //Implement text if answer is correct/incorrect along with sounds
+
+//TO DO: Time subtracts from timer if answered incorrectly
+
+//TO DO: Game is over when when all questions are answered or timer reaches 0
+
+//TO DO: Save initials and score when game is over
